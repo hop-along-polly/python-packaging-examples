f77b1adddda59a2db3c41f887f92d52356bded10
Derek Drummond <ddrummond@cylance.com>
2020-06-11 23:30:38 -0500

    resolve ModuleNotFoundError when using pytest

---
+++ example-4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='pytranslator', # Names the package. pip list will show pytranslator
+  version='0.1.0', # it's best practice to assign a version #
+  packages=find_packages() # Searches the working directory for folders containing python packages
+)
